68d46ceab5ca2d4dac835bf08527a02919ed87e9
alamedahealthcare <abramibrahem22@gmail.com>
2025-12-08 13:32:33 +0200

    create home screen UI

---
+++ lib/features/home/presentation/screens/home_screen.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hungry/core/constant/app_colors.dart';
+import 'package:hungry/core/constant/app_string.dart';
+import 'package:hungry/core/helpers/spacing.dart';
+
+import 'package:hungry/core/widget/custom_text.dart';
+import 'package:hungry/core/widget/search_field.dart';
+import 'package:hungry/features/home/presentation/screens/food_card_item.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                  children: [
+                    verticalSpacing(40),
+                    UserDataHeaderSection(username: 'Hello, Abram Ibrahim'),
+                    verticalSpacing(20),
+                    SearchField(controller: TextEditingController()),
+                    verticalSpacing(20),
+                    HorizontalCategoriesList(
+                      categories: ["Pizza", "Burger", "Fries", "Salad"],
+                    ),
+                    verticalSpacing(20),
+                  ],
+                ),
+              ),
+            ),
+            SliverPadding(
+              padding: EdgeInsets.symmetric(horizontal: 24.0),
+              sliver: SliverGrid(
+                delegate: SliverChildBuilderDelegate(childCount: 10, (
+                  context,
+                  index,
+                ) {
+                  return FoodCardItem();
+                }),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class HorizontalCategoriesList extends StatefulWidget {
+  final List<String> categories;
+
+  const HorizontalCategoriesList({super.key, required this.categories});
+
+  @override
+  State<HorizontalCategoriesList> createState() =>
+      _HorizontalCategoriesListState();
+}
+
+class _HorizontalCategoriesListState extends State<HorizontalCategoriesList> {
+  ValueNotifier<int?> currentIndex = ValueNotifier(null);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children: List.generate(widget.categories.length, (index) {
+          return GestureDetector(
+            onTap: () {
+              if (currentIndex.value == index) {
+                setState(() {
+                  currentIndex.value = null;
+                });
+              } else {
+                setState(() {
+                  currentIndex.value = index;
+                });
+              }
+            },
+            child: Container(
+              width: 150,
+              height: 50,
+              margin: EdgeInsets.only(right: 10, bottom: 8),
+              decoration: BoxDecoration(
+                color: currentIndex.value == index
+                    ? AppColors.primaryColor
+                    : AppColors.categoryCardColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Center(
+                child: CustomText(
+                  text: widget.categories[index],
+                  size: 16,
+                  color: currentIndex.value == index
+                      ? AppColors.whiteColor
+                      : Colors.grey,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class UserDataHeaderSection extends StatelessWidget {
+  final String username;
+
+  const UserDataHeaderSection({super.key, required this.username});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SvgPicture.asset(
+              width: 30,
+              height: 30,
+              AppAssets.hungry,
+              color: AppColors.primaryColor,
+            ),
+            verticalSpacing(5),
+            CustomText(
+              text: username,
+              size: 24,
+              color: Colors.grey.shade500,
+              fontWeight: FontWeight.normal,
+            ),
+          ],
+        ),
+
+        Container(
+          decoration: BoxDecoration(),
+          child: Image.asset(AppAssets.sonicImg, fit: BoxFit.fill),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/features/home/root/root_screen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:hungry/core/constant/app_colors.dart';
+import 'package:hungry/features/auth/ui/profile_screen.dart';
+import 'package:hungry/features/card/presentation/card_screen.dart';
+import 'package:hungry/features/home/presentation/screens/home_screen.dart';
+import 'package:hungry/features/order/presentation/order_history_screen.dart';
+
+class RootScreen extends StatefulWidget {
+  const RootScreen({super.key});
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> {
+  final PageController pageController = PageController();
+  int currentIndex = 0;
+  List<Widget> pageScreens = [
+    const HomeScreen(),
+    const CardScreen(),
+    const OrderHistoryScreen(),
+    const ProfileScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: PageView(
+        controller: pageController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: pageScreens,
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: AppColors.primaryColor,
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(30), topRight: Radius.circular(30))
+        ),
+        child: BottomNavigationBar(
+          elevation: 0,
+            onTap: (index) {
+              setState(() => currentIndex = index);
+              pageController.jumpToPage(currentIndex);
+            },
+            currentIndex: currentIndex,
+            type: BottomNavigationBarType.fixed,
+            backgroundColor: Colors.transparent,
+            selectedItemColor: AppColors.whiteColor,
+            unselectedItemColor: Colors.grey.shade700,
+
+            items: [
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'Home'),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.cart), label: 'Cart'),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.home), label: 'Order History'),
+              BottomNavigationBarItem(icon: Icon(CupertinoIcons.profile_circled), label: 'Profile'),
+
+            ]
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
